b840a57f0301555465a1066dbf5a26e8ca84d446
gharam37 <g_z48@yahoo.com>
2018-02-11 13:59:47 +0200

    lots of features XD created table as a .ser file .. waiting for ur page component to make it 0.ser instead of student.ser .. check the main method and run it .. it's an example

--- DBapp/src/DbApp.java
+++ DBapp/src/DbApp.java
@@ -3,10 +3,30 @@ import java.util.Hashtable;
 
 public class DbApp {
 	
-	public void createTable(String strTableName, //to do Create Table
+	public static void createTable(String strTableName, //to do Create Table
 			String strClusteringKeyColumn,
 			Hashtable<String,String> htblColNameType )
 			throws DBAppException {
+		new Table(strTableName,strClusteringKeyColumn,htblColNameType );
+		
+		
+		
+	}
+	public static void main(String[]args){
+		String strTableName = "Student";
+		@SuppressWarnings("rawtypes")
+		Hashtable htblColNameType = new Hashtable( );
+		htblColNameType.put("id", "java.lang.Integer");
+		htblColNameType.put("name", "java.lang.String");
+		htblColNameType.put("gpa", "java.lang.double");
+		
+		try {
+			
+			createTable( strTableName, "id", htblColNameType );
+		} catch (DBAppException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 		
--- DBapp/src/Table.java
+++ DBapp/src/Table.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Hashtable;
 
@@ -14,20 +19,79 @@ public class Table implements Serializable  {
 	
 	String strClusteringKeyColumn; //table primary key
 	String strTableName; //table name
+	private static final long serialVersionUID = 1L;
+	
 	Hashtable<String,String> htblColNameType; // hashtable of the attributes and their types.. to put inserted in a metadata file
 	public Table(String strTableName,
 			String strClusteringKeyColumn, 
 			Hashtable<String,String> htblColNameType){ //to do the exception 
+		this.strTableName=strTableName;
+		this.strClusteringKeyColumn=strClusteringKeyColumn;
+		this.htblColNameType=htblColNameType;
 		
-		
-		MakeMeta(htblColNameType);
+		try {
+			MakeMeta(htblColNameType);
+			
+			System.out.println("Created");
+		} catch (DBAppException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 	
-	public void MakeMeta( Hashtable<String,String> htblColNameTypehtblColNameType){
+	public void MakeMeta ( Hashtable<String,String> htblColNameTypehtblColNameType)throws DBAppException{
 		
-		////////////ToDo Write index info into a MetaData File
+	 
+		ObjectOutputStream ObjectOutputStream;
+		try {
+			ObjectOutputStream = new ObjectOutputStream(new FileOutputStream(
+			new File(this.strTableName + ".ser"))); //intially writing the table as a big fat serializable thing .. make this our first page XD ..  when we merge i'll be put it in the array of pages
+			ObjectOutputStream.writeObject(this);   /// instead of .strtablename it will be 0.ser
+			ObjectOutputStream.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
-	}
-}
+		/*createIndex(strTableName, strKeyColName);
+		try {
+			FileWriter writer = new FileWriter("metadata.csv");
+
+			Set<Entry<String, String>> set1 = htblColNameType.entrySet();
+			Iterator<Entry<String, String>> iterator1 = set1.iterator();
+			while (iterator1.hasNext()) {
+				Entry<String, String> en = iterator1.next();
+				writer.append(strTableName);
+				writer.append(en.getKey());
+				writer.append(en.getValue());
+				if (strKeyColName.equals(en.getKey())) {
+					writer.append("True");
+				} else {
+					writer.append("False");
+				}
+				writer.append("False");
+				if (htblColNameRefs.contains(en.getKey())) {
+					writer.append(htblColNameRefs.get(en.getKey()));
+				} else {
+					writer.append("null");
+				}
+				writer.append("\n");
+			}
+
+			// generate whatever data you want
+
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		
+		
+	}*/
+}}
